bb1723693dab686b193b3dc8fe318c65781b551f
Willem Liu <willem_liu@hotmail.com>
2020-03-24 16:31:07 +0100

    Added footer and css modules

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Menu } from "../src/Menu";
+import { Footer } from "../src/Footer";
 
 function Index(props: any) {
     return (
@@ -7,6 +8,7 @@ function Index(props: any) {
             <Menu />
             <h1>{props.id ? `ID: ${props.id}` : "Home"}</h1>
             {props.title && <p>Title: {props.title}</p>}
+            <Footer />
         </>
     );
 }
