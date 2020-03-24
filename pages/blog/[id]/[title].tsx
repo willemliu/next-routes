a9785901e7fb04bb0e187f6eced13084eb08dac8
Willem Liu <willem_liu@hotmail.com>
2020-03-24 16:27:35 +0100

    Using same index file for blog

--- pages/blog/[id]/[title].tsx
+++ pages/blog/[id]/[title].tsx
@@ -1,19 +1,2 @@
-import React from "react";
-import { Menu } from "../../../src/Menu";
-
-function BlogPost(props: any) {
-    return (
-        <>
-            <Menu />
-            <h1>ID: {props.id}</h1>
-            <p>Title: {props.title}</p>
-        </>
-    );
-}
-
-BlogPost.getInitialProps = (context) => {
-    console.log(context.query, context.params);
-    return { ...context.query };
-};
-
+import BlogPost from "../../index";
 export default BlogPost;
